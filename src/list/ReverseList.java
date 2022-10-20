a03d37880b44f308b4d6d2a6b607f8c15da5227e
JJPeng <pengjiajun755@163.com>
2022-10-20 23:00:17 +0800

    反转链表

--- src/list/ReverseList.java
+++ src/list/ReverseList.java
@@ -12,21 +12,17 @@ public class ReverseList {
         }
     }
 
-    /**
-     * 反转单链表
-     * @param head 链表头节点
-     * @return 新头节点
-     */
+    //反转单链表
     public static Node reverseLinkedList(Node head) {
         Node pre = null;
         Node next = null;
-        while (head != null) {//第一步，先想到遍历整个链表,所以想到使用while循环
-            next = head.next;//因为反转需要将next指针指向前一个节点，但又需要不断向后遍历，所以先将下一个节点的位置保存起来
-            head.next = pre;//修改next指针达到反转的目的
-            pre = head;//在移动到下一个节点之前，需要记录下一个节点的前一个节点
-            head = next;//来到下一个节点
+        while(head != null) {
+            next = head.next;//反转前先记住下一个的位置
+            head.next = pre;//反转
+            pre = head;//记住当前位置，作为下一个节点反转的next
+            head = next;//准备反转下一个节点
         }
-        return pre;//当head移动到null时，pre指向的就是最后一个节点的位置
+        return pre;
     }
 
 
@@ -39,15 +35,12 @@ public class ReverseList {
         }
     }
 
-    /**
-     * 反转双向链表
-     * @param head 链表头节点
-     * @return 新头节点
-     */
+
+    //反转双向链表
     public static TwoWayNode reverseTwoWayLinkedList(TwoWayNode head) {
         TwoWayNode pre = null;
         TwoWayNode next = null;
-        while (head != null) {
+        while(head != null) {
             next = head.next;
             head.next = pre;
             head.pre = next;
