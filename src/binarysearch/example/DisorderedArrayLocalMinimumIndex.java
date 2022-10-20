d8e20311820dd4114cf62db9675205d9d90aadc3
JJPeng <pengjiajun755@163.com>
2022-10-20 21:53:38 +0800

    二分查找无序数组的局部最小值

--- src/binarysearch/example/DisorderedArrayLocalMinimumIndex.java
+++ src/binarysearch/example/DisorderedArrayLocalMinimumIndex.java
@@ -3,8 +3,8 @@ package binarysearch.example;
 /**
  * 给定一个无序数组，任意两个相邻的数都不相等。返回其中的任意一个局部最小值的位置就行。
  */
-public class BSLocalMinimum {
-    public static int searchLocalMinimum(int[] arr) {
+public class DisorderedArrayLocalMinimumIndex {
+    public static int searchLocalMinimumIndex(int[] arr) {
         if (arr == null || arr.length < 1) {
             return -1;
         } else if (arr.length == 1) {
