b5ace1a8f9286fc4508efbb5f9a8fdc1152ecaa8
JJPeng <pengjiajun755@163.com>
2022-10-20 19:05:10 +0800

    随机整型数组产生器

---
+++ src/other/IntegerArrayGenerator.java
@@ -0,0 +1,18 @@
+package other;
+
+/**
+ * @author JJPeng
+ * @date 2022/10/20 19:00
+ */
+public class IntegerArrayGenerator {
+
+    public static int[] generate(int maxSize, int maxValue) {
+        int randomSize = (int) (Math.random() * (maxSize + 1));
+        int [] arr = new int[randomSize];
+        for (int i = 0; i < randomSize; i++) {
+            arr[i] = (int) (Math.random() * (maxValue + 1));
+        }
+        return arr;
+    }
+
+}
