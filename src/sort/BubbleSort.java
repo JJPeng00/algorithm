b2971c6f9fc2604ea128514297676f8c97e0eeb3
JJPeng <pengjiajun755@163.com>
2022-10-20 19:59:24 +0800

    重写冒泡排序

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -14,14 +14,14 @@ public class BubbleSort {
         if (arr == null || arr.length < 2) {
             return;
         }
+
         for (int i = arr.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    swap(arr, j, j + 1);
-                }
+                if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
             }
         }
     }
+
     private static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
